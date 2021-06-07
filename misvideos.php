9fc25f08921bdcd71e1c1464846defae12a5eb6c
YOYO2112 <84356508+YOYO2112@users.noreply.github.com>
2021-06-07 14:45:17 -0400

    Add files via upload

---
+++ misvideos.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.0
+ */
+
+/**
+ * Database `videos`
+ */
+
+/* `videos`.`misvideos` */
+$misvideos = array(
+  array('id' => '1','nombre' => 'Chapa de corcho','sinopsis' => 'Ejemplo 1','url' => 'misvideos/1.mp4'),
+  array('id' => '2','nombre' => 'Dibujos xl','sinopsis' => 'Ejemplo 2','url' => 'misvideos/2.mp4'),
+  array('id' => '3','nombre' => 'Chapa de corcho','sinopsis' => 'Ejemplo 1','url' => 'misvideos/1.mp4'),
+  array('id' => '4','nombre' => 'Dibujos xl','sinopsis' => 'Ejemplo 2','url' => 'misvideos/2.mp4')
+);
+?>
